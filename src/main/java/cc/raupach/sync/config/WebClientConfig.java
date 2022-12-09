e304536d0e52311ee2dda2a4640317892cf79b25
Oliver Raupach <oliver@raupach.cc>
2022-12-09 18:54:58 +0100

    spring 6

--- src/main/java/cc/raupach/sync/config/WebClientConfig.java
+++ src/main/java/cc/raupach/sync/config/WebClientConfig.java
@@ -3,16 +3,23 @@ package cc.raupach.sync.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableWebSecurity
 public class WebClientConfig {
 
     @Value("${sync.shopware.token-uri}")
@@ -28,32 +35,45 @@ public class WebClientConfig {
     ReactiveClientRegistrationRepository clientRegistrations() {
 
         ClientRegistration registration = ClientRegistration
-                .withRegistrationId("sw")
-                .tokenUri(tokenUri)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
-                .build();
+          .withRegistrationId("sw")
+          .tokenUri(tokenUri)
+          .clientId(clientId)
+          .clientSecret(clientSecret)
+          .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+          .build();
         return new InMemoryReactiveClientRegistrationRepository(registration);
     }
 
+    @Bean
+    public OAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository, OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+        OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+          .clientCredentials()
+          .build();
+
+        DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository);
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+        return authorizedClientManager;
+    }
+
     @Bean(name = "shopware")
-    WebClient shopwareWebClient(ReactiveClientRegistrationRepository clientRegistrations) {
-        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth = new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, new UnAuthenticatedServerOAuth2AuthorizedClientRepository());
-        oauth.setDefaultClientRegistrationId("sw");
+    WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
+        oauth2Client.setDefaultClientRegistrationId("custom");
 
         return WebClient.builder()
-                .filter(oauth)
-                .exchangeStrategies(ExchangeStrategies.builder()
-                        .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
-                        .build())
-                .build();
+          .apply(oauth2Client.oauth2Configuration())
+          .exchangeStrategies(ExchangeStrategies.builder()
+            .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
+            .build())
+          .build();
     }
 
     @Bean(name = "printful")
     WebClient printfulWebClient() {
 
         return WebClient.builder()
-                .build();
+          .build();
     }
 }
